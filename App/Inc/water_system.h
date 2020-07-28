de2afbd2b5bd07126d659cf85e30be7115a4a745
Abdelmonem Mostafa <48067992+abdelmonem17@users.noreply.github.com>
2020-07-28 02:15:29 +0200

    Update water_system.h

--- App/Inc/water_system.h
+++ App/Inc/water_system.h
@@ -1,5 +1,5 @@
-#ifndef __MYSYSTEM_H__
-#define __MYSYSTEM_H__
+#ifndef __WATER_SYSTEM_H__
+#define __WATER_SYSTEM_H__
 
 
 /****************************************************************************
@@ -11,72 +11,5 @@
 void systemInit(void);
 
 
-
-/****************************************************************************
- *
- *   @brief function acts as task and polls the status of the ON/OFF display button
- *   @params  void
- *
- *
- *   @return  void
- ****************************************************************************/
-void btnOnOffMonitoring_Task(void);
-
-
-
-
-
-/****************************************************************************
- *
- *   @brief function polls up and down buttons to set temperture
- *   @params  void
- *
- *
- *   @return  void
- ****************************************************************************/
-void btnsUpDownMonitoring_Task(void);
-
-
-/****************************************************************************
- *
- *   @brief function checks cooling and heating status and accordingly turn the heater
- *    or cooler on or off
- *   @params  void
- *
- *
- *   @return  void
- ****************************************************************************/
-void btnOnOffMonitoring_Task(void);
-
-/****************************************************************************
- *
- *   @brief function acts as function polls the state of current temperature
- *   comparing to required one
- *   @params  void
- *
- *   @return  void
- ****************************************************************************/
-void tempertureMonitoring_Task(void);
-
-
-/****************************************************************************
- *
- *   @brief function acts as a task and polls the status of seven segements
- *   @params  void
- *
- *
- *   @return  void
- ****************************************************************************/
-void SevenSegmentDisplay_Task(void);
-
-/****************************************************************************
- *
- *   @brief function acts as a task and polls the eeprom to write on it
- *   @params  void
- *
- *
- *   @return  void
- ****************************************************************************/
-void EEPROM_Task(void);
 
 #endif
